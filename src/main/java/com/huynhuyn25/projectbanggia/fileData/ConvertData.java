c79e0668b83da8a6a4f7f9caa5b6797333862a0f
HuynHuyn25 <huyn.nn258@gmail.com>
2022-08-11 18:51:32 +0700

    convert message - 10/8

--- src/main/java/com/huynhuyn25/projectbanggia/fileData/ConvertData.java
+++ src/main/java/com/huynhuyn25/projectbanggia/fileData/ConvertData.java
@@ -2,6 +2,8 @@ package com.huynhuyn25.projectbanggia.fileData;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.huynhuyn25.projectbanggia.model.StockInfo;
+import com.huynhuyn25.projectbanggia.model.TopPrice;
 import quickfix.*;
 import quickfix.field.Message;
 import quickfix.field.MessageFactory;
@@ -13,48 +15,69 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ConvertData {
-    public void ConvertStO(File file) throws IOException, ConfigError, InvalidMessage, FieldNotFound {
+    ArrayList<StockInfo> listStockInfo = new ArrayList<>();
+    public String ConvertStO(String line) throws IOException, ConfigError, InvalidMessage, FieldNotFound {
         DataDictionary dictionary = new DataDictionary("src/main/resources/CustomFIX.xml");
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        int count = 0;
-        Gson gson = new Gson();
-        String line;
-//        BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/dataTP.txt"));
-        while ((line = reader.readLine()) != null) {
-            count++;
-//            quickfix.MessageFactory messageFactory = new DefaultMessageFactory(ApplVerID.FIX44);
-//            quickfix.Message messageFromMessageUtils = quickfix.MessageUtils.parse(messageFactory, null, line);
-            Message messageFromMessageUtils = (Message) MessageUtils.parse(new MessageFactory(), dictionary, line, false);
-            List<JsonObject> listJsonData = new ArrayList<>();
-            JsonObject jsonObject = new JsonObject();
-            String msgType = ((Message.Header) messageFromMessageUtils.getHeader()).getMsgType().getValue();
-//            System.out.println(((Message.Header) messageFromMessageUtils.getHeader()).getSendingTime());
-            jsonObject.addProperty("MsgType",msgType);
-            Iterator<Field<?>> iterator = messageFromMessageUtils.iterator();
-            while (iterator.hasNext()) {
-                Field<?> field = iterator.next();
-                jsonObject.addProperty(dictionary.getFieldName(field.getField()) != null ? dictionary.getFieldName(field.getField()) : "" + field.getTag() + "", (String) (field.getObject() != null ? field.getObject() : ""));
-//                jsonObject.addProperty(dictionary.getFieldName(field.getField()), (String) field.getObject());
-//                System.out.println(dictionary.getFieldName(field.getField()) + ": " + field.getObject());
+        Message messageFromMessageUtils = (Message) MessageUtils.parse(new MessageFactory(), dictionary, line, false);
+        List<JsonObject> listJsonData = new ArrayList<>();
+        JsonObject jsonObject = new JsonObject();
+        String msgType = ((Message.Header) messageFromMessageUtils.getHeader()).getMsgType().getValue();
+        jsonObject.addProperty("MsgType",msgType);
+        Iterator<Field<?>> iterator = messageFromMessageUtils.iterator();
+        while (iterator.hasNext()) {
+            Field<?> field = iterator.next();
+            jsonObject.addProperty(dictionary.getFieldName(field.getField()) != null ? dictionary.getFieldName(field.getField()) : "" + field.getTag() + "", (String) (field.getObject() != null ? field.getObject() : ""));
+        }
+        listJsonData.add(jsonObject);
+        Iterator<Integer> iteratorKeys = messageFromMessageUtils.groupKeyIterator();
+        int key;
+        while (iteratorKeys.hasNext()) {
+            key = iteratorKeys.next();
+            for (Group group : messageFromMessageUtils.getGroups(key)) {
+                iterator = group.iterator();
+                jsonObject = new JsonObject();
+                while (iterator.hasNext()) {
+                    Field<?> field = iterator.next();
+                    jsonObject.addProperty(dictionary.getFieldName(field.getField()) != null ? dictionary.getFieldName(field.getField()) : "" + field.getTag() + "", (String) (field.getObject() != null ? field.getObject() : ""));
+                }
+                listJsonData.add(jsonObject);
             }
-            listJsonData.add(jsonObject);
-            Iterator<Integer> iteratorKeys = messageFromMessageUtils.groupKeyIterator();
-            int key;
-            while (iteratorKeys.hasNext()) {
-                key = iteratorKeys.next();
-                for (Group group : messageFromMessageUtils.getGroups(key)) {
-                    iterator = group.iterator();
-                    jsonObject = new JsonObject();
-                    while (iterator.hasNext()) {
-                        Field<?> field = iterator.next();
-                        jsonObject.addProperty(dictionary.getFieldName(field.getField()) != null ? dictionary.getFieldName(field.getField()) : "" + field.getTag() + "", (String) (field.getObject() != null ? field.getObject() : ""));
-                    }
-                    listJsonData.add(jsonObject);
+            }
+        String messageSend;
+        if(msgType.equals("SI")) messageSend = convertSI(listJsonData);
+        else if(msgType.equals("TP")) messageSend = convertTP(listJsonData);
+        else return null;
+
+        return messageSend;
+
+    }
+
+    private String convertTP(List<JsonObject> listJsonData) {
+        Gson gson = new Gson();
+        JsonObject jsonObject = listJsonData.get(0);
+        if(listStockInfo.size()!=0){
+            String symbol = subString(gson.toJson(jsonObject.get("Symbol")));
+            for (StockInfo stockInfo:listStockInfo){
+                if(symbol.equals(stockInfo.getSymbol())){
+
                 }
             }
-
-
         }
+        return gson.toJson(listJsonData);
+    }
 
+    private String convertSI(List<JsonObject> listJsonData) {
+        Gson gson = new Gson();
+        JsonObject jsonObject = listJsonData.get(0);
+        StockInfo stockInfo = new StockInfo(subString(gson.toJson(jsonObject.get("Symbol"))),
+                subString(gson.toJson(jsonObject.get("BasicPrice"))),
+                subString(gson.toJson(jsonObject.get("CeilingPrice"))),
+                subString(gson.toJson(jsonObject.get("FloorPrice"))));
+        listStockInfo.add(stockInfo);
+        return gson.toJson(stockInfo);
+    }
+
+    private String subString(String s){
+        return s.substring(1,s.length()-1);
     }
 }
--- src/main/java/com/huynhuyn25/projectbanggia/ws/MyHandler.java
+++ src/main/java/com/huynhuyn25/projectbanggia/ws/MyHandler.java
@@ -9,10 +9,13 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import quickfix.ConfigError;
+import quickfix.DataDictionary;
 import quickfix.FieldNotFound;
 import quickfix.InvalidMessage;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.List;
 import java.util.random.*;
 import java.io.IOException;
@@ -20,20 +23,24 @@ import java.util.ArrayList;
 
 @Slf4j
 public class MyHandler extends TextWebSocketHandler {
-    List<List<JsonObject>> test = new ArrayList<>();
+
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException, InterruptedException, ConfigError, InvalidMessage, FieldNotFound {
         log.info(message.toString());
-        ConvertData convertData = new ConvertData();
-        convertData.ConvertStO(new File("src/main/resources/infogateclient.20200529.dat"));
-        int k =0;
-        while(k < test.size()){
 
-            session.sendMessage(new TextMessage(test.get(k).toString()));
-            k++;
-            Thread.sleep((long) (Math.random()*10000));
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/infogateclient.20200529.dat"));
+        String line;
+        while ((line = reader.readLine()) != null){
+            ConvertData convertData = new ConvertData();
+            String messageFromData = convertData.ConvertStO(line);
+            if(messageFromData!= null) {
+                session.sendMessage(new TextMessage(messageFromData));
+                Thread.sleep((long) (Math.random() * 10000));
+            }
         }
+
+
     }
 
 }
